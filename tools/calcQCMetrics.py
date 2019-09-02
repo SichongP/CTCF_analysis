fab66a99b99bbcf38352e28a192e1ee586d3665a
SichongP <scpeng@ucdavis.edu>
2019-09-02 14:19:06 -0700

    Rule to calculate quality metrics

--- tools/calcQCMetrics.py
+++ tools/calcQCMetrics.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+import subprocess
 
-outfile = snakemake.output
+outfile = snakemake.output[0]
 
 samples = snakemake.params.samples
 
 with open(outfile, 'w') as out:
-    out.write("Tissue\tRep\tDepth\tNRF\tPBC1\tPBC2\tNSC\tRSC\tJSD\tSJSD\tIJSD")
+    out.write("Tissue\tRep\tDepth\tNRF\tPBC1\tPBC2\tNSC\tRSC\tJSD\tSJSD\tIJSD\n")
     for sample in samples:
         rep = sample.split('_')[0]
         tissue = sample.split('_')[1]
@@ -13,15 +14,15 @@ with open(outfile, 'w') as out:
         depth = int(subprocess.check_output(["samtools","view","-c","Results/mapping/{}_markdup.bam".format(sample)]))
         result = subprocess.check_output(["bamToBed -i Results/mapping/{}_filtered.bam | awk 'BEGIN{{OFS=\"\t\"}}{{print $1,$2,$3,$6}}' | sort | uniq -c | awk 'BEGIN{{mt=1;m0=1;m1=1;m2=1}} ($1==1){{m1=m1+1}} ($1==2){{m2=m2+1}} {{m0=m0+1}} {{mt=mt+$1}} END{{printf \"%d\t%d\t%d\t%d\t%f\t%f\t%f\",mt,m0,m1,m2,m0/mt,m1/m0,m1/m2}}'".format(sample)], shell = True)
         metrics = result.split()
-        output.extend([depth,float(metrics[4]),float(metrics[5]),float(metrics[6])])
+        output.extend([str(depth),str(float(metrics[4])),str(float(metrics[5])),str(float(metrics[6]))])
         with open("Results/metrics/spp/{}_spp_stats.txt".format(sample)) as sppf:
             sppstats = sppf.read().split()
-        output.extend([float(sppstats[8]), float(sppstats[9])])
-        with open("Results/metrics/{}_LibraryQC.tsv".format) as f:
+        output.extend([str(sppstats[8]), str(sppstats[9])])
+        with open("Results/metrics/{}_LibraryQC.tsv".format(sample)) as f:
             lines = f.readlines()
         chipstats = lines[1].split()
         try:
-            sjsd = float(shipstats[8])
+            sjsd = float(chipstats[8])
         except IndexError:
             sjsd = 0
         if len(lines) > 2:
@@ -29,7 +30,8 @@ with open(outfile, 'w') as out:
             ijsd = float(lines[2].split()[8])
             if ijsd > sjsd:
                 jsd = -jsd
-            output.extend([jsd, sjd, ijsd])
+            output.extend([str(jsd), str(sjsd), str(ijsd)])
         else:
-            output.extend(["null",sjsd,"null"])
+            output.extend(["N/A",sjsd,"N/A"])
         out.write('\t'.join(output))
+        out.write('\n')
